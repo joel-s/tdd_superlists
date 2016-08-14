1183d5db081397dc69dc062be00eb89dc09ead20
Joel Sullivan <joel-s@users.noreply.github.com>
2016-08-14 16:38:11 -0400

    FT now checks that we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -10,22 +11,36 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Fred visits the to-do list home page
         self.browser.get("http://localhost:8000")
 
-        # He observes that the title say something about To-Do lists
+        # He observes that the title and <h1> say something about To-Do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Test not fully implemented')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # The page invites him to enter a to-do item
+        inputbox = self.browser.find_element_by_id('new_item_box')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # He types "Think about what to do"
+        inputbox.send_keys("Think about what to do")
 
         # When he types Enter, the page updates and now lists
         # "1: Think about..."
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('todo_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1. Think about what to do' for row in rows)
+        )
 
         # There is still a text box inviting him to add another item
+        self.fail("Finish the test!")
 
         # He enters "Make a more detailed to-do list" and presses Enter
 
