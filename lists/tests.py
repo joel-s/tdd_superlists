9ede41a6d9d8df81fee7e4016291d27ea5c92439
Joel Sullivan <joel-s@users.noreply.github.com>
2016-08-19 17:28:51 -0400

    Home page redirects and supports multiple items (FT fails)

--- lists/tests.py
+++ lists/tests.py
@@ -26,13 +26,42 @@ class HomePageTest(TestCase):
         request.POST['item_text'] = 'A new list item'
 
         response = home_page(request)
-        self.assertIn('A new list item', response.content.decode())
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        self.assertEqual(response.status_code, 302)   # redirect
+        self.assertEqual(response['location'], '/')
+
+
+    def test_home_page_redirects_after_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
 
 
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
         response = home_page(request)
         self.assertEqual(Item.objects.count(), 0)
+
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='MyItem 1')
+        Item.objects.create(text='MyItem 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('MyItem 1', response.content.decode())
+        self.assertIn('MyItem 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
