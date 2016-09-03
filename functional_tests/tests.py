6b20c99d1306a40dc84ad1c8ed3076f09b358667
Joel Sullivan <joel-s@users.noreply.github.com>
2016-09-02 20:53:01 -0400

    Fleshed out FT to expect new URL (and content) for each visitor

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -40,8 +40,10 @@ class NewVisitorTest(LiveServerTestCase):
         # He types "Think about what to do"
         inputbox.send_keys("Think about what to do", Keys.ENTER)
 
-        # When he types Enter, the page updates and now lists
-        # "1: Think about..."
+        # When he types Enter, he is taken to a new URL. The page updates and
+        # now lists "1: Think about..."
+        fred_list_url = self.browser.current_url
+        self.assertRegex(fred_list_url, 'lists/.+')
         self.assert_rows_in_list_table(
             '1. Think about what to do',
         )
@@ -57,8 +59,6 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys("Make a more detailed to-do list", Keys.ENTER)
 
         # The page updates again, and now shows both items on the list
-        table = self.browser.find_element_by_id('todo_list_table')
-        rows = table.find_elements_by_tag_name('tr')
         self.assert_rows_in_list_table(
             '1. Think about what to do',
             '2. Make a more detailed to-do list',
@@ -66,6 +66,36 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Fred sees a statement that the site has generated a unique URL for
         # his list
-        self.fail('Test not fully implemented')
+        ## TODO: Implement this
 
         # Fred visits the URL, and sees that his to-do list is there
+        ## TODO: Implement this
+
+        # Now a new user, Wilma, comes along to the site
+        ## We use a new browser session to make sure none of Fred's info is
+        ## coming through from cookies etc.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Wilma visits the home page--no sign of Fred's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Think about what to do', page_text)
+        self.assertNotIn('Make a more detailed to-do list', page_text)
+
+        # Wilma starts a new list by entering an item
+        inputbox = self.browser.find_element_by_id('new_item_box')
+        inputbox.send_keys("Restore peace to the Middle East", Keys.ENTER)
+
+        # Wilma gets her own unique URL
+        wilma_list_url = self.browser.current_url
+        self.assertRegex(wilma_list_url, 'lists/.+')
+        self.assertNotEqual(fred_list_url, wilma_list_url)
+
+        # Wilma's new page lists her item but not Fred's
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertIn("Restore peace to the Middle East", page_text)
+        self.assertNotIn('Think about what to do', page_text)
+        self.assertNotIn('Make a more detailed to-do list', page_text)
+
+        # Satisfied, they both go back to sleep
